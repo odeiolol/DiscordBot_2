2015f5d97031e3c3502012586098ab766f6c84aa
unknown <ttttheguard@gmail.com>
2021-03-16 12:59:17 -0300

    add coffe function

--- disc.py
+++ disc.py
@@ -10,6 +10,8 @@ class MyClient(discord.Client):
 
         if message.content=='ping':
             await message.channel.send('pong')
+        if message.content=='café':
+            await message.channel.send(':coffee:')
 
 client=MyClient()
 client.run('NzYzNzc5MDMyMDE3MjcyODQ0.X38qpw.0fJuwMSI920jaRR0CST4LnQaIEk')
