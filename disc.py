eaa4c505af2d3337d871e251569eb26dd5c1d7e1
unknown <ttttheguard@gmail.com>
2021-03-16 12:45:46 -0300

    first commit

---
+++ disc.py
@@ -0,0 +1,15 @@
+import discord 
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content=='ping':
+            await message.channel.send('pong')
+
+client=MyClient()
+client.run('NzYzNzc5MDMyMDE3MjcyODQ0.X38qpw.0fJuwMSI920jaRR0CST4LnQaIEk')
